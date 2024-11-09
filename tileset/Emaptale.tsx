72238721c4c8c0b20b00c27a8bb154a7d1750b77
ThalisAFalqueto <thalisambrosimfalqueto@gmail.com>
2024-11-09 20:40:38 -0300

    finalmente o mapa aparece corretamente, após refazer

---
+++ tileset/Emaptale.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Emaptale" tilewidth="20" tileheight="20" tilecount="900" columns="30">
+ <image source="EMApTale_Sprites_Remade.png" width="600" height="600"/>
+</tileset>
