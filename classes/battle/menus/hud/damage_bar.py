63652b241333074219e78d24c7fbd2a373f90321
jaopredo <joao.profissional.45@gmail.com>
2024-11-11 11:32:26 -0300

    Agora a barra que indica o ataque para

--- classes/battle/menus/hud/damage_bar.py
+++ classes/battle/menus/hud/damage_bar.py
@@ -19,26 +19,41 @@ class DamageBar(pygame.sprite.Sprite):
                 self.sprites[i],
                 (
                     self.sprites[i].get_width(),
-                    container.inner_rect.height
+                    pygame.display.get_surface().get_height()*0.3
                 )
             )
 
         self.image = self.sprites[0]
         self.rect = self.image.get_rect()
-        self.rect.centery = container.inner_rect.centery
-        self.rect.centerx = container.inner_rect.centerx
+
+        self.container = container
 
         self.random_dir = 0
-        self.speed = 5
+        self.speed = 10
 
         self.animated = False
 
         self.execution_counter = 0
+
     
     def choose_direction(self):
         self.random_dir = choice([1, -1])
+        self.rect.centerx = self.container.inner_rect.centerx + (self.container.inner_rect.width/2)*self.random_dir
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_z] or keys[pygame.K_RETURN]:
+            self.entered_pressing = True
 
     def update(self, *args, **kwargs):
         # self.rect.x += self.speed * self.random_dir
 
+        keys = pygame.key.get_pressed()
+
+        if (keys[pygame.K_z] or keys[pygame.K_RETURN]) and not self.entered_pressing:
+            self.random_dir = 0
+            self.entered_pressing = True
+
+        if not keys[pygame.K_RETURN] and not keys[pygame.K_z]:
+            self.entered_pressing = False
+
         self.rect.x += self.speed*self.random_dir*-1
+        self.rect.centery = self.container.inner_rect.centery
