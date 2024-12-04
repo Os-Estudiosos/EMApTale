4291eefc354818ffd64fc2375fbe9c5b34fbfd16
Bruno Ferreira <brunoferreira17k@gmail.com>
2024-12-04 20:43:57 -0300

    finalizados 3

--- screens/cutscene/gameover_cutscene.py
+++ screens/cutscene/gameover_cutscene.py
@@ -104,7 +104,7 @@ class GameoverCutscene(State):
         for event in EventManager.events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_BACKSPACE or event.key == pygame.K_SPACE:
-                    self.__game_state_manager.set_state('final_cutscene')
+                    self.__game_state_manager.set_state('start')
                     SoundManager.stop_music()
 
         pygame.display.flip()
